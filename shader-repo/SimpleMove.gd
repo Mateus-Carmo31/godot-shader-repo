c88678646696ded15399b9908e4dc141fc6ebb87
Mateus-Carmo31 <mateus_oliveira31@outlook.com>
2020-06-10 12:16:39 -0300

    Improved the systems to work with the physics system instead of just meshes. Just need to make the mesh look good now...

--- shader-repo/SimpleMove.gd
+++ shader-repo/SimpleMove.gd
@@ -1,14 +1,17 @@
-extends MeshInstance
+extends KinematicBody
 
 export(NodePath) var mask_viewport
 export(float) var speed = 4.0
 
+var cull_mesh : Spatial
+
 func _ready():
-	var cull_mesh = $CullMesh
+	cull_mesh = $CullMesh
 	remove_child(cull_mesh)
 	get_node(mask_viewport).add_child(cull_mesh)
 	$RemoteTransform.remote_path = cull_mesh.get_path()
 
+var velocity := Vector3.ZERO
 func _process(delta):
 	
 	var forward = - get_viewport().get_camera().global_transform.basis.z
@@ -25,15 +28,14 @@ func _process(delta):
 	
 	movement_dir = (forward * ver + right * hor).normalized()
 	
-	translation += movement_dir * delta * speed
+	velocity = move_and_slide(movement_dir * speed)
+
+func _physics_process(delta):
+	var space_state = get_world().direct_space_state
 	
-#	update_mask_position()
-#
-#func update_mask_position():
-#
-#	var screen_pos = get_viewport().get_camera().unproject_position(translation)
-#	screen_pos = Vector2(screen_pos.x, screen_pos.y) / get_viewport().size
-#
-#	if mask_texture:
-#		var node = get_node(mask_texture)
-#		node.material.set("shader_param/position", screen_pos)
+	var result = space_state.intersect_ray(
+		global_transform.origin, 
+		get_viewport().get_camera().global_transform.origin, 
+		[self])
+	
+	$RemoteTransform.scale = Vector3.ONE if result else Vector3.ZERO
